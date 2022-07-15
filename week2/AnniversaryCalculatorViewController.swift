0debcf352dd05bccb3d54d87b58a52f69777d5f6
minseongkim97 <kyrok67@naver.com>
2022-07-15 09:34:40 +0900

    기념일 계산 로직 추가

--- week2/AnniversaryCalculatorViewController.swift
+++ week2/AnniversaryCalculatorViewController.swift
@@ -9,18 +9,18 @@ import UIKit
 
 class AnniversaryCalculatorViewController: UIViewController {
     //MARK: - Properties
-    let calendar = Calendar.current
-    let currentDate = Date()
-
-    @IBOutlet var dateLabel: [UILabel]!
+    let ddays = [100, 200, 300 ,400]
+    @IBOutlet var dateLabels: [UILabel]!
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        changeDate(date: Date())
     }
     
     //MARK: - Action
     @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        changeDate(date: sender.date)
     }
     
     //MARK: - Helpers
@@ -31,5 +31,21 @@ class AnniversaryCalculatorViewController: UIViewController {
         dateFormatter.dateFormat = "yyyy년 MM월 dd일"
         
         return dateFormatter.string(from: date)
+    }
+    
+    func calculateDate(date: Date) -> [Date] {
+        var dates = [Date]()
+        let calendar = Calendar.current
+        for dday in ddays {
+            dates.append(calendar.date(byAdding: .day, value: dday, to: date)!)
+        }
+        
+        return dates
+    }
+    
+    func changeDate(date: Date) {
+        for i in 0..<dateLabels.count {
+            dateLabels[i].text = dateToFormattedString(date: calculateDate(date: date)[i])
+        }
     }
 }
